8114605fe751b957f0a43dde12293970344fae06
slogvo <86196042+slogvo@users.noreply.github.com>
2025-03-10 11:54:14 +0700

    add icon

--- src/utils/mappers.js
+++ src/utils/mappers.js
@@ -1,22 +1,32 @@
-const { slugify } = require("./slugify");
+const { slugify } = require('./slugify');
 
 /**
  * Converts post data from Notion to API format
  */
 const mapPostData = async (page, blocks = null) => {
+  console.log('🚀 ~ mapPostData ~ page:', page);
   const properties = page.properties;
 
   // Basic
   const post = {
     id: page.id,
-    title: properties.Name?.title?.[0]?.plain_text || "Untitled",
-    cover: page?.cover?.external?.url || properties.Cover?.files?.[0]?.file?.url || null,
-    slug: properties.Slug?.rich_text?.[0]?.plain_text || slugify(properties.Name?.title?.[0]?.plain_text || "untitled"),
-    status: properties.Status?.select?.name ||  properties.Status?.status?.name || "Draft",
+    icon: page.icon,
+    title: properties.Name?.title?.[0]?.plain_text || 'Untitled',
+    cover:
+      page?.cover?.external?.url ||
+      properties.Cover?.files?.[0]?.file?.url ||
+      null,
+    slug:
+      properties.Slug?.rich_text?.[0]?.plain_text ||
+      slugify(properties.Name?.title?.[0]?.plain_text || 'untitled'),
+    status:
+      properties.Status?.select?.name ||
+      properties.Status?.status?.name ||
+      'Draft',
     publishDate: properties.PublishDate?.date?.start || null,
-    // featuredImage: properties.FeaturedImage?.files?.[0]?.file?.url || null,  
-    excerpt: properties.Excerpt?.rich_text?.[0]?.plain_text || "",
-    author: properties.Author?.select?.name
+    // featuredImage: properties.FeaturedImage?.files?.[0]?.file?.url || null,
+    excerpt: properties.Excerpt?.rich_text?.[0]?.plain_text || '',
+    author: properties.Author?.select?.name,
   };
 
   // Tags/Categories
@@ -35,10 +45,9 @@ const mapPostData = async (page, blocks = null) => {
     const categoryIds = properties.Category.relation.map((rel) => rel.id);
     post.categoryIds = categoryIds;
 
-  // Normally, you would want to fetch additional information about the category here
-  // However, this would increase the number of API calls
-  // Therefore, we only return the ID, and the frontend will fetch the detailed information later
-
+    // Normally, you would want to fetch additional information about the category here
+    // However, this would increase the number of API calls
+    // Therefore, we only return the ID, and the frontend will fetch the detailed information later
   } else {
     post.categoryIds = [];
   }
@@ -47,8 +56,7 @@ const mapPostData = async (page, blocks = null) => {
   if (properties.Author?.relation?.length > 0) {
     const authorId = properties.Author.relation[0].id;
     post.authorId = authorId;
-  // Similarly, we only return the author ID
-
+    // Similarly, we only return the author ID
   } else {
     post.authorId = null;
   }
@@ -66,13 +74,13 @@ const mapPostData = async (page, blocks = null) => {
  * Convert author data from Notion to API format
  */
 const mapAuthorData = async (page) => {
-  console.log("🚀 ~ mapAuthorData ~ page:", page)
+  console.log('🚀 ~ mapAuthorData ~ page:', page);
   const properties = page.properties;
 
   return {
     id: page.id,
-    name: properties.Name?.title?.[0]?.plain_text || "Unknown Author",
-    bio: properties.Bio?.rich_text?.[0]?.plain_text || "",
+    name: properties.Name?.title?.[0]?.plain_text || 'Unknown Author',
+    bio: properties.Bio?.rich_text?.[0]?.plain_text || '',
     avatar: properties.Avatar?.files?.[0]?.file?.url || null,
     email: properties.Email?.email || null,
     twitter: properties.Twitter?.url || null,
@@ -89,10 +97,10 @@ const mapCategoryData = (page) => {
 
   return {
     id: page.id,
-    name: properties.Name?.title?.[0]?.plain_text || "Uncategorized",
-    description: properties.Description?.rich_text?.[0]?.plain_text || "",
-    slug: properties.Slug?.rich_text?.[0]?.plain_text || "uncategorized",
-    color: properties.Color?.select?.name || "default",
+    name: properties.Name?.title?.[0]?.plain_text || 'Uncategorized',
+    description: properties.Description?.rich_text?.[0]?.plain_text || '',
+    slug: properties.Slug?.rich_text?.[0]?.plain_text || 'uncategorized',
+    color: properties.Color?.select?.name || 'default',
   };
 };
 
@@ -105,12 +113,12 @@ const mapSettingsData = (page) => {
   const properties = page.properties;
 
   return {
-    siteName: properties.SiteName?.title?.[0]?.plain_text || "My Blog",
+    siteName: properties.SiteName?.title?.[0]?.plain_text || 'My Blog',
     siteDescription:
-      properties.SiteDescription?.rich_text?.[0]?.plain_text || "",
+      properties.SiteDescription?.rich_text?.[0]?.plain_text || '',
     logo: properties.Logo?.files?.[0]?.file?.url || null,
     favicon: properties.Favicon?.files?.[0]?.file?.url || null,
-    footerText: properties.FooterText?.rich_text?.[0]?.plain_text || "",
+    footerText: properties.FooterText?.rich_text?.[0]?.plain_text || '',
     socialLinks: {
       twitter: properties.Twitter?.url || null,
       facebook: properties.Facebook?.url || null,
